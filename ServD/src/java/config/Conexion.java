f1808a604ccd293e4c82c767b2e5b0043fa6fdcb
ocastillo-2021185 <oacc04castillo@gmail.com>
2023-09-13 09:28:58 -0600

    Se hizo merge a la main de la rama caltan-2019176 para presentar el proyecto final en clase

--- ServD/src/java/config/Conexion.java
+++ ServD/src/java/config/Conexion.java
@@ -11,7 +11,7 @@ public class Conexion {
     public Connection Conexion(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBServD?useSSL=false","root", "admin"); //Cambian la contraseña :3
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBServD?useSSL=false","root", "password"); //Cambian la contraseña :3
         }catch(Exception e){
             e.printStackTrace();
         }
